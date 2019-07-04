7e3d0c85344534e07a819f88cce0d4ab9e9f869b
Bilal Shahzad (66110) <bilal.shahzad@ibexglobal.com>
2019-07-04 20:48:45 +0500

    Configurations Demo

---
+++ ConfigurationsDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConfigurationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<String, String> inMemory = new Dictionary<string, string>();
+            inMemory.Add("level", "100");
+
+            //Create Builder Object
+            var builder = new ConfigurationBuilder();
+
+            //Set BasePath so it knows where to find files
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+            //Add multiple configuration sources
+            //The ones added late in this, take precedence
+            builder
+                .AddJsonFile("mysettings.json", true, true)
+                .AddJsonFile("prod_settings.json", true, true)
+                .AddXmlFile("test_settings.config", true, true)
+                .AddInMemoryCollection(inMemory)
+                //.AddEnvironmentVariables()  //It will include all Environment variables
+                .AddEnvironmentVariables("MYAPP_") //It will include only those which are starting with MYAPP_ prefix
+                .AddCommandLine(args);
+
+            //IConfigurationRoot
+            var configBuild = builder.Build();
+
+            //Print all first level child
+            foreach (var c in configBuild.GetChildren())
+            {
+                Console.WriteLine($"Key:{c.Key}, Value:{c.Value}, Path:{c.Path}");
+            }
+
+            //IConfigurationSection
+            var dbsection = configBuild.GetSection("database");
+
+            //Get a specific section
+            Console.WriteLine("---------Children of database section");
+
+            //Print all first level child (of this section)
+            foreach (var c in dbsection.GetChildren())
+            {
+                Console.WriteLine($"Key:{c.Key}, Value:{c.Value}, Path:{c.Path}");
+            }
+
+            Console.WriteLine("--------------------------");
+            //You can access an element by using flattend key
+            Console.WriteLine($"DBName via key: {configBuild["database:dbname"]}");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # DotNetCore
+
+## Configurations Demo
+### What are we going to learn?
+- Load Configuration from Json file
+- Load Configuration from XML file
+- Load Configuration from In Memory
+- Load Configuration from Environment Variables
+- Load Configuration from Command Line Arguments
+- Configuration Precedence
+
+**Steps**
+1) Create a .NET Core Console Application
+2) Install following Packages
+Microsoft.Extensions.Configuration
+Microsoft.Extensions.Configuration.Json
+Microsoft.Extensions.Configuration.Xml
+Microsoft.Extensions.Configuration.EnvironmentVariables
+Microsoft.Extensions.Configuration.CommandLine
+
+3) Add Two Json files (Right click on project -> Add -> New - Json File.
+settings.json (Click on file and change "Copy to output Directory" => "Copy Always")
+prod_settings.json (Click on file and change "Copy to output Directory" => "Copy Always")
+4) Provide following configurations in JSON format
+
+Here is content of first file (**settings.json**)
+
+       {
+          "cst": "testing12345",
+          "level": "1",
+          "database": {
+            "dbname": "test",
+            "userid": "sa",
+            "password": "123"
+          }
+        }
+
+and here is content of second file (**prod_settings.json**)
+
+    {
+      "database": {
+        "dbname": "db_prod",
+        "userid": "sa",
+        "password": "12345"
+      }
+    }
+5) Add New XML file (Right click on project -> Add -> New - App configuration (i.e. test_settings.config). (Click on file and change "Copy to output Directory" => "Copy Always")
+6) Add following content in XML file
+First line should be following     
+> <?xml version="1.0" encoding="utf-8" ?>
+Other content is
+
+    <root>
+      <level>5</level>
+      <database>
+        <dbname>test2</dbname>
+        <userid>sa2</userid>
+        <password>12345</password>
+      </database>
+    </root>
+
+7) Check & add Code from program.cs file
+8) You may now run & build the application to check output or you may go the folder which project (.csproj) exists and Open CMD there
+To build & run the application
+> dotnet run 
+
+To Pass arguments
+> dotnet run --level=3
+
+To create an environment variable
+> SET MYAPP_DATA=3
