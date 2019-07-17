942c87f717ac7160dae3fa6fe52e67d6101ec6f2
Bilal Shahzad (66110) <bilal.shahzad@ibexglobal.com>
2019-07-17 16:58:43 +0500

    ASPNET_Core_Demos

---
+++ ASPNETCore_DI_Demo/ASPNETCore_Demos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCore_Demos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
